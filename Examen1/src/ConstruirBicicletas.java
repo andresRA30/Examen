cd9d16769f8e9b776360d47874dccd43580a70d0
Andres <andrexrobert@gmail.com>
2019-04-05 19:08:44 -0600

    cambio

--- Examen1/src/ConstruirBicicletas.java
+++ Examen1/src/ConstruirBicicletas.java
@@ -4,8 +4,9 @@ public class ConstruirBicicletas {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 BicicletaCarrera BC = new BicicletaCarrera(new MarcoL() , new Suspension(), new  MonoPlato());
-BicicletaMTB BM= new BicicletaMTB(new MarcoL() , new Suspension(), new  MonoPlato()); 	
 BC.tipoDeBicicleta();
+System.out.println("");
+BicicletaMTB BM= new BicicletaMTB(new MarcoM() , new Fijo(), new  Biplato()); 	
 BM.tipoDeBicicleta();
 
 	}
